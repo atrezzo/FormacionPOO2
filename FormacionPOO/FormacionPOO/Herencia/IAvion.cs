b48aa0761093a60b35488a5d44f1ea228664f38d
atrezzo <mamorenolao@gmail.com>
2016-09-01 09:49:22 +0200

    Inclusión ficheros de interfaces.

---
+++ FormacionPOO/FormacionPOO/Herencia/IAvion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FormacionPOO.Herencia
+{
+    interface IAvion : IVehiculo
+    {
+        int MaximaAltitud { get; }
+        int NumeroMotores { get; }
+        int VelocidadAterrizaje { get; }
+        int VelocidadDespegue { get; }
+        bool Volando { get; }
+
+        void Aterrizar();
+        void Despegar();
+    }
+}
---
+++ FormacionPOO/FormacionPOO/Herencia/ICoche.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FormacionPOO.Herencia
+{
+    public interface ICoche : IVehiculo
+    {
+        string Matricula { get; }
+        Motor Motor { get; }
+        int NumeroPuertas { get; }
+        int NumeroRuedas { get; }
+
+        void Matricular(string matricula);
+    }
+}
